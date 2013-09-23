2dda3b2a31a869a2138cff6dfe4f5b33d797863a
TechFounder <techfounder@outlook.com>
2013-09-22 23:23:38 -0400

    Add ability and update the applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  private
+
+  def current_ability
+  	@current_ability ||= case
+                       when current_user
+                         UserAbility.new(current_user)
+                       when current_admin
+                         AdminAbility.new(current_admin)
+                       when current_company_user
+                         CompanyUserAbility.new(current_company_user)
+                       else
+                         GuestAbility.new
+                       end  
+                   end
 end
