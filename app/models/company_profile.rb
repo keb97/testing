77387e4d64c60976ccb4343f7110c91416fb8d54
TechFounder <techfounder@outlook.com>
2013-09-28 23:57:29 -0400

    Add validation to vehicle and company profile models

--- app/models/company_profile.rb
+++ app/models/company_profile.rb
@@ -1,4 +1,6 @@
 class CompanyProfile < ActiveRecord::Base
 	has_many :company_users, :dependent => :delete_all
 	has_many :vehicles
+
+	validates :description, presence: true
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ActiveRecord::Base
 	belongs_to :company
+
+	validates :description, presence: true
 end
