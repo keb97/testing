6264d7c199b205b3753960f13b84ca8ea7c8f98a
TechFounder <techfounder@outlook.com>
2013-10-04 10:15:38 -0400

    Fix some errors in test views

--- db/migrate/20130925125204_add_company_profile_id_to_vehicles.rb
+++ db/migrate/20130925125204_add_company_profile_id_to_vehicles.rb
@@ -1,6 +1,6 @@
 class AddCompanyProfileIdToVehicles < ActiveRecord::Migration
   def change
-    add_column :vehicles, :company_profile_id, :integer
-    add_index :vehicles, :company_profile_id
+    add_column	:vehicles, :company_profile_id, :integer
+    add_index		:vehicles, :company_profile_id
   end
 end
--- spec/views/company_profiles/new.html.erb_spec.rb
+++ spec/views/company_profiles/new.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "company_profiles/new" do
   before(:each) do
     assign(:company_profile, stub_model(CompanyProfile,
+      :name => "MyText",
       :description => "MyText",
       :photo => ""
     ).as_new_record)
@@ -13,6 +14,7 @@ describe "company_profiles/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", company_profiles_path, "post" do
+      assert_select "input#company_profile_name[name=?]", "company_profile[name]"
       assert_select "textarea#company_profile_description[name=?]", "company_profile[description]"
       assert_select "input#company_profile_photo[name=?]", "company_profile[photo]"
     end
